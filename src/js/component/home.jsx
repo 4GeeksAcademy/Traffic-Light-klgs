ee7d62f467650d8018803f1853dff02c57a82301
klgs1234 <138398036+klgs1234@users.noreply.github.com>
2023-08-09 20:23:51 +0000

    boton de alternar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,6 +8,7 @@ const [rojo,setRojo] = useState("secondary")
 const [amarillo,setAmarillo] = useState("secondary")
 
 
+
 function handleTrafficLightVerde(){
  setRojo("secondary")
  setAmarillo("secondary")
@@ -23,14 +24,24 @@ function handleTrafficLightRed(){
   setAmarillo("warning")
   setVerde("secondary")
  }
+ function handleNewColor() {
+  if (rojo === "danger") {
+    handleTrafficLightAmarillo();
+  } else if (amarillo === "warning") {
+    handleTrafficLightVerde();
+  } else {
+    handleTrafficLightRed();
+  }
+}
 
 
   return (
     <> 
-    <div className="container-fluid ">
+  
+    <div className="container-fluid">
       <div className="semaforo justify-content-center" style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "150px", border: "1px solid gray", background: "gray"}} >
    
-  <button className= {"btn btn-" + rojo + " p-5" + " rounded-circle" } onClick={handleTrafficLightRed} >
+  <button className= {"btn btn-" + rojo + " p-5" + " rounded-circle" } onClick={handleTrafficLightRed}>
   
     </button>
   <button className= {"btn btn-" + amarillo + " p-5" + " rounded-circle"} onClick={handleTrafficLightAmarillo} >
@@ -39,8 +50,15 @@ function handleTrafficLightRed(){
     <button className= {"btn btn-" + verde + " p-5" + " rounded-circle"} onClick={handleTrafficLightVerde}>
 		
     </button>
+
       </div>
-    </div>  
+    </div> 
+    <div className="alternar ">
+    <button
+  className={"btn btn-primary m-5"}
+  onClick={handleNewColor}>Alternar
+</button>
+      </div> 
    </> 
   );
 };
